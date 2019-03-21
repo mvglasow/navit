43bf1e045ba49ebc26e0ddfc4eb2c53e181d3b0e
mvglasow <michael -at- vonglasow.com>
2019-03-21 17:49:28 +0100

    WIP:Refactor:core:Add thread library
    Signed-off-by: mvglasow <michael -at- vonglasow.com>

---
+++ navit/thread.c
@@ -0,0 +1,192 @@
+/**
+ * Navit, a modular navigation system.
+ * Copyright (C) 2005-2019 Navit Team
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Library General Public License
+ * version 2 as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU Library General Public
+ * License along with this program; if not, write to the
+ * Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+ * Boston, MA  02110-1301, USA.
+ */
+
+/** @file
+ *
+ * @brief Helper definitions and macros for working with GLib threads.
+ *
+ * This file contains convenience macros for thread-related functions, mostly for the purpose of making the code easier
+ * to read by avoiding excessive conditionals. It should be included immediately after `glib.h`.
+ *
+ * Callers should use `#if HAVE_GLIB_THREADS` (defined in this header) instead of `#ifdef G_THREADS_ENABLED`. This
+ * makes it easier to disable multithreading in one single place and build a single-threaded version of Navit even on a
+ * platform with multithreading support. (Note that even in a single-threaded build, third-party libraries may still
+ * spawn additional threads.)
+ *
+ * It is perfectly OK to use the standard GLib functions instead of the shorthands defined here, as long as they are
+ * wrapped in an `#if HAVE_GLIB_THREADS` condition.
+ *
+ * Where macros occur immediately before or after other portions which need to be wrapped in an `#if HAVE_GLIB_THREADS`
+ * condition, they can be placed inside or outside the condition. For the sake of code legibility, consistency is
+ * recommended: place macros outside conditions where possible, and if you need to place one macro inside the
+ * condition, do the same for its counterpart. Avoid adding extra conditionals.
+ */
+
+#include "thread.h"
+#include <glib/gmem.h>
+#ifndef HAVE_API_WIN32_BASE
+#include <errno.h>
+#include <time.h>
+#endif
+#include "debug.h"
+
+#if HAVE_POSIX_THREADS
+/**
+ * @brief Describes the main function for a thread.
+ */
+struct thread_main_data {
+    int (*main)(void *);       /**< The thread’s main function. */
+    void * data;               /**< The argument for the function. */
+};
+
+/**
+ * @brief A wrapper around the main thread function.
+ *
+ * This wraps the implementation-neutral main function into a function with the signature expected by the POSIX thread
+ * library.
+ *
+ * @param data Pointer to a `struct thread_main_data` encapsulating the main function and its argument.
+ */
+static void *thread_main_wrapper(void * data) {
+    struct thread_main_data * main_data = (struct thread_main_data *) data;
+    void * ret = (void *) (main_data->main(main_data->data));
+    return ret;
+}
+#endif
+
+thread *thread_new(int (*main)(void *), void * data, char * name) {
+#if HAVE_POSIX_THREADS
+    int err;
+    thread * ret = g_new0(thread, 1);
+    struct thread_main_data * main_data = g_new0(struct thread_main_data, 1);
+    main_data->main = main;
+    err = pthread_create(ret, NULL, thread_main_wrapper, (void *) main_data);
+    if (err) {
+        dbg(lvl_error, "error %d, thread=%p", err, ret);
+        g_free(ret);
+        return NULL;
+    }
+#ifdef __USE_GNU
+    if (name) {
+        err = pthread_setname_np(*thread, name);
+        if (err)
+            dbg(lvl_warning, "error %d, thread=%p", err, ret);
+    }
+#endif
+    return ret;
+#else
+#error "call to thread_new() on a platform without thread support"
+#endif
+}
+
+void thread_destroy(thread* thread) {
+#if HAVE_POSIX_THREADS
+    g_free(thread);
+#endif
+}
+
+void thread_sleep(long msec) {
+#ifdef HAVE_API_WIN32_BASE
+    Sleep(msec);
+#else
+    struct timespec req, rem;
+    req.tv_sec = (msec /1000);
+    req.tv_nsec = (msec % 1000) * 1000000;
+    while ((nanosleep(&req, &rem) == -1) && errno == EINTR)
+        req = rem;
+#endif
+}
+
+void thread_exit(int result) {
+#if HAVE_POSIX_THREADS
+    pthread_exit((void *) result);
+#else
+    return;
+#endif
+}
+
+int thread_join(thread * thread) {
+#if HAVE_POSIX_THREADS
+    void * ret;
+    int err = pthread_join(*thread, &ret);
+    if (err) {
+        dbg(lvl_error, "error %d, thread=%p", err, thread);
+        return -1;
+    }
+    return (int) ret;
+#else
+    return -1;
+#endif
+}
+
+thread_lock *thread_lock_new() {
+#if HAVE_POSIX_THREADS
+    thread_lock *ret = g_new0(thread_lock, 1);
+    int err = pthread_rwlock_init(ret, NULL);
+    if (err) {
+        dbg(lvl_error, "error %d, lock=%p", err, ret);
+        g_free(ret);
+        return NULL;
+    }
+    return ret;
+#else
+    return NULL;
+#endif
+}
+
+void thread_lock_destroy(thread_lock *lock) {
+#if HAVE_POSIX_THREADS
+    int err = pthread_rwlock_destroy(lock);
+    if (err)
+        dbg(lvl_error, "error %d, lock=%p", err, lock);
+    g_free(lock);
+#endif
+}
+
+void thread_lock_acquire_read(thread_lock *lock) {
+#if HAVE_POSIX_THREADS
+    int err = pthread_rwlock_rdlock(lock);
+    if (err)
+        dbg(lvl_error, "error %d, lock=%p", err, lock);
+#endif
+}
+
+void thread_lock_release_read(thread_lock *lock) {
+#if HAVE_POSIX_THREADS
+    int err = pthread_rwlock_unlock(lock);
+    if (err)
+        dbg(lvl_error, "error %d, lock=%p", err, lock);
+#endif
+}
+
+void thread_lock_acquire_write(thread_lock *lock) {
+#if HAVE_POSIX_THREADS
+    int err = pthread_rwlock_wrlock(lock);
+    if (err)
+        dbg(lvl_error, "error %d, lock=%p", err, lock);
+#endif
+}
+
+void thread_lock_release_write(thread_lock *lock) {
+#if HAVE_POSIX_THREADS
+    int err = pthread_rwlock_unlock(lock);
+    if (err)
+        dbg(lvl_error, "error %d, lock=%p", err, lock);
+#endif
+}
